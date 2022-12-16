6d867b6026e76a6e59d84f5145aac29c2f0c5986
Satish Nayak <satish_nayak@epam.com>
2022-12-16 13:13:57 -0500

    Performance example 1

---
+++ PerformanceExample1/src/com/instanceofcake/performance/Main.java
@@ -0,0 +1,12 @@
+package com.instanceofcake.performance;
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PrimeNumbers primeNumbers = new PrimeNumbers();
+		Integer max = Integer.parseInt(args[0]);
+		primeNumbers.generateNumbers(max);
+	}
+
+}
